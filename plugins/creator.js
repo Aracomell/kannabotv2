6bbc5a85c7d6858ad83eb838247084ff597cf75b
Aracomell <109452970+Aracomell@users.noreply.github.com>
2022-10-21 15:09:09 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
+*ɴᴀᴍᴇ:* Kevin Alfarel
+*ᴀɢᴇ:* 19ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
 *ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Rangkasbitung
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
